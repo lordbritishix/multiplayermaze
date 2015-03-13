4c698c0906b61cbe9f6d8aea70b23a8de2b5fe8f
Jim Quitevis <jim.quitevis@globalrelay.net>
2015-03-12 17:13:47 -0700

    Ansible and Docker

---
+++ server/src/main/java/com/quitevis/multiplayermaze/server/App.java
@@ -0,0 +1,15 @@
+package com.quitevis.multiplayermaze.server;
+
+/**
+ * Server entry point
+ * 
+ * @author jim.quitevis
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello From Server!" );
+    }
+}
