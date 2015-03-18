bf882d976a6147a6cb7be9b16ab7fb4e07fa9ab9
Jim Quitevis <jim.quitevis@globalrelay.net>
2015-03-18 12:40:57 -0700

    Allowed deploy user connection to postgres

--- docker/db/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "host all  all    10.0.0.0/8  trust" >> $PGDATA/pg_hba.conf
-echo "host all  all    192.168.0.0/16  trust" >> $PGDATA/pg_hba.conf
-
-#For production, only listen to selected ip addresses
-echo "listen_addresses='*'" >> $PGDATA/postgresql.conf
-
-#service postgresql restart
-#psql --command "CREATE USER deploy WITH ENCRYPTED PASSWORD 'deploy' CREATEDB CREATEUSER CREATEROLE"
